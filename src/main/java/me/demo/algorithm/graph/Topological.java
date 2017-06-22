34b0e8e3c51955009f1f31621646d27360e9dcaa
wlingxiao <wlingxiao@outlook.com>
2017-06-22 22:28:22 +0800

    topological sort

---
+++ src/main/java/me/demo/algorithm/graph/Topological.java
@@ -0,0 +1,25 @@
+package me.demo.algorithm.graph;
+
+/**
+ * 拓扑排序
+ */
+public class Topological {
+
+    private Iterable<Integer> order;
+
+    public Topological(Digraph digraph) {
+        DirectedCycle cycleFinder = new DirectedCycle(digraph);
+        if (!cycleFinder.hasCycle()) {
+            DepthFirstOrder dfs = new DepthFirstOrder(digraph);
+            order = dfs.reversePost();
+        }
+    }
+
+    public Iterable<Integer> order() {
+        return order;
+    }
+
+    public boolean isDAG() {
+        return order != null;
+    }
+}
