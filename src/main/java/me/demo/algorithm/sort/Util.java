3e49e971ff2ff65d7885d311bb18ae60863067de
wlingxiao <wlingxiao@outlook.com>
2017-08-02 15:12:02 +0800

    沉迷上班，日渐消瘦(乖巧) -- 选择排序

---
+++ src/main/java/me/demo/algorithm/sort/Util.java
@@ -0,0 +1,16 @@
+package me.demo.algorithm.sort;
+
+public final class Util {
+    private Util() {
+    }
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+}
