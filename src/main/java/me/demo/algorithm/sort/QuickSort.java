ce91cb0f63075af1ccd92a34099ea5a4178e1bfc
wlingxiao <wlingxiao@outlook.com>
2017-08-03 22:19:14 +0800

    打球出了一身汗 -- 快速排序

---
+++ src/main/java/me/demo/algorithm/sort/QuickSort.java
@@ -0,0 +1,44 @@
+package me.demo.algorithm.sort;
+
+import static me.demo.algorithm.sort.Util.exch;
+import static me.demo.algorithm.sort.Util.less;
+
+public class QuickSort {
+
+    public static void sort(Comparable[] a) {
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi < lo) {
+            return;
+        }
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        Comparable v = a[lo];
+        while (true) {
+            while (less(a[++i], v)) {
+                if (i == hi) {
+                    break;
+                }
+            }
+            while (less(v, a[--j])) {
+                if (j == lo) {
+                    break;
+                }
+            }
+            if (i >= j) {
+                break;
+            }
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+}
