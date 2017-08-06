5e5015a53e8cbadcf04e35f2aef8b81b98cd9686
wlingxiao <wlingxiao@outlook.com>
2017-08-06 19:33:23 +0800

    希尔排序

---
+++ src/main/java/me/demo/algorithm/sort/Shell.java
@@ -0,0 +1,24 @@
+package me.demo.algorithm.sort;
+
+import static me.demo.algorithm.sort.Util.exch;
+import static me.demo.algorithm.sort.Util.less;
+
+public class Shell {
+
+    public static void sort(Comparable[] c) {
+        int N = c.length;
+        int h = 1;
+        while (h < N / 3) {
+            h = h * 3;
+        }
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h && less(c[j], c[j - h]); j -= h) {
+                    exch(c, j, j - h);
+                }
+            }
+            h /= 3;
+        }
+    }
+
+}
