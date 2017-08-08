898199b2cc5ffe6bfdb0c3db8e5f26cf9d309fcd
wlingxiao <wlingxiao@outlook.com>
2017-08-08 23:35:22 +0800

    三向切分的快速排序

---
+++ src/main/java/me/demo/algorithm/sort/Quick3way.java
@@ -0,0 +1,29 @@
+package me.demo.algorithm.sort;
+
+import static me.demo.algorithm.sort.Util.exch;
+
+public class Quick3way {
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int lt = lo;
+        int i = lo + 1;
+        int gt = hi;
+        Comparable v = a[lo];
+        while (i <= gt) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) {
+                exch(a, lt++, i++);
+            } else if (cmp > 0) {
+                exch(a, i, gt--);
+            } else {
+                i++;
+            }
+        }
+        sort(a, lo, gt - 1);
+        sort(a, gt + 1, hi);
+    }
+
+}
