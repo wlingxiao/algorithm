a77b3708d800eb01ba01a53c538ddd52b11565a2
wlingxiao <wlingxiao@outlook.com>
2017-08-10 22:14:37 +0800

    update merge sort

--- src/main/java/me/demo/algorithm/sort/Merge.java
+++ src/main/java/me/demo/algorithm/sort/Merge.java
@@ -6,6 +6,21 @@ public class Merge {
 
     private static Comparable[] aux;
 
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    public static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+
     public static void merge(Comparable[] a, int lo, int mid, int hi) {
         int i = lo;
         int j = mid + 1;
