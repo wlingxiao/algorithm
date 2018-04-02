028967ccc982adfdad40cfd4f99e9cdc955856c3
wlingxiao <wlingxiao@outlook.com>
2018-04-02 20:39:26 +0800

    black red tree

---
+++ src/main/java/me/demo/algorithm/tree/BlackRedTree.java
@@ -0,0 +1,16 @@
+package me.demo.algorithm.tree;
+
+/**
+ * 1. 每个节点要么是红的，要么是黑的
+ * 2. 根节点是黑的
+ * 3. 每个叶子节点是黑的
+ * 4. 如果一个节点是红的，那么他的两个儿子是黑的
+ * 5. 对任一节点而言，其到叶节点尾端NULL指针的每一掉路径都包含相同数目的黑节点
+ * <p>
+ * 参考文章：
+ * https://github.com/julycoding/The-Art-Of-Programming-By-July/blob/master/ebook/zh/03.01.md
+ * https://blog.csdn.net/eson_15/article/details/51144079
+ */
+public class BlackRedTree {
+
+}
