61526636aaa94607a8b818ad464c5a12545ea8c6
wlingxiao <wlingxiao@outlook.com>
2018-08-13 20:42:54 +0800

    use jmh for benchmark

---
+++ src/main/java/me/demo/algorithm/sort/SortBenchmarkEntry.java
@@ -0,0 +1,18 @@
+package me.demo.algorithm.sort;
+
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class SortBenchmarkEntry {
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(".*" + SortBenchmark.class.getSimpleName() + ".*")
+                .forks(1)
+                .build();
+        new Runner(opt).run();
+    }
+
+}
