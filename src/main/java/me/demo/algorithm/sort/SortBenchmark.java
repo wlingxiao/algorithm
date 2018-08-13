adfeadc36d65224224dd94f5ff931f76f0318c74
wlingxiao <wlingxiao@outlook.com>
2018-08-13 20:57:51 +0800

    add java Arrays sort for comparision

--- src/main/java/me/demo/algorithm/sort/SortBenchmark.java
+++ src/main/java/me/demo/algorithm/sort/SortBenchmark.java
@@ -2,6 +2,7 @@ package me.demo.algorithm.sort;
 
 import org.openjdk.jmh.annotations.*;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import static me.demo.algorithm.sort.Util.integerArray;
@@ -43,6 +44,15 @@ public class SortBenchmark {
     @Measurement(iterations = 5, time = 5)
     public void testMergeSort(Data data) {
         Merge.sort(data.integers);
+    }
+
+    @Benchmark
+    @Warmup(iterations = 1)
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Measurement(iterations = 5, time = 5)
+    public void testArraysSort(Data data) {
+        Arrays.sort(data.integers);
     }
 
 }
