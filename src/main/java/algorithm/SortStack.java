336ee415c038b149709b8b104079f4c29273cbe3
wlingxiao <wlingxiao@outlook.com>
2025-09-03 15:08:23 +0800

    增加 SortStack  图例和解决思路

--- src/main/java/algorithm/SortStack.java
+++ src/main/java/algorithm/SortStack.java
@@ -1,24 +1,24 @@
 package algorithm;
 
 import algorithm.list.Stack;
+import lombok.val;
 
-/**
- * Given a stack of integers, sort it in ascending order using another temporary stack.
- * Examples:
- * <pre>
- *  Input : [34, 3, 31, 98, 92, 23]
- *  Output : [3, 23, 31, 34, 92, 98]
- *
- *  Input : [3, 5, 1, 4, 2, 8]
- *  Output : [1, 2, 3, 4, 5, 8]
- * </pre>
- */
+/// - 图例在 `docs/SortStack.drawio`
+/// - Given a stack of integers, sort it in ascending order using another temporary stack.
+/// - Examples:
+/// - Input : `[34, 3, 31, 98, 92, 23]` Output : `[ 3, 23, 31, 34, 92, 98]`
+/// - Input : `[3, 5, 1, 4, 2, 8]` Output : `[ 1, 2, 3, 4, 5, 8]`
 public class SortStack {
 
+    /// ##### 解决思路
+    /// 汉诺塔问题
+    /// 1. 弹出 stack 元素，如果 temp 为空则直接压入该元素
+    /// 2. 如果 temp 不为空，则检查 temp 顶部元素是否小于该元素，如果是则弹出 temp 顶部元素压入 stack，重复执行该操作，直到 temp 被清空。
+    /// 3. 最后将该元素压入 temp
     public Stack<Integer> sort(Stack<Integer> stack) {
-        Stack<Integer> temp = new Stack<>();
+        val temp = new Stack<Integer>();
         while (!stack.isEmpty()) {
-            Integer i = stack.pop();
+            val i = stack.pop();
             while (!temp.isEmpty() && temp.top() < i) {
                 stack.push(temp.pop());
             }
--- src/test/java/algorithm/SortStackTests.java
+++ src/test/java/algorithm/SortStackTests.java
@@ -1,9 +1,10 @@
 package algorithm;
 
 import algorithm.list.Stack;
-import org.junit.Test;
+import lombok.val;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SortStackTests {
 
@@ -18,13 +19,13 @@ public class SortStackTests {
         stack.push(3);
         stack.push(34);
 
-        Stack<Integer> ret = sortStack.sort(stack);
-        assertEquals(ret.pop().intValue(), 3);
-        assertEquals(ret.pop().intValue(), 23);
-        assertEquals(ret.pop().intValue(), 31);
-        assertEquals(ret.pop().intValue(), 34);
-        assertEquals(ret.pop().intValue(), 92);
-        assertEquals(ret.pop().intValue(), 98);
+        val ret = sortStack.sort(stack);
+        assertEquals(3, ret.pop());
+        assertEquals(23, ret.pop());
+        assertEquals(31, ret.pop());
+        assertEquals(34, ret.pop());
+        assertEquals(92, ret.pop());
+        assertEquals(98, ret.pop());
     }
 
     @Test
@@ -38,13 +39,13 @@ public class SortStackTests {
         stack.push(5);
         stack.push(3);
 
-        Stack<Integer> ret = sortStack.sort(stack);
-        assertEquals(ret.pop().intValue(), 1);
-        assertEquals(ret.pop().intValue(), 2);
-        assertEquals(ret.pop().intValue(), 3);
-        assertEquals(ret.pop().intValue(), 4);
-        assertEquals(ret.pop().intValue(), 5);
-        assertEquals(ret.pop().intValue(), 8);
+        val ret = sortStack.sort(stack);
+        assertEquals(1, ret.pop());
+        assertEquals(2, ret.pop());
+        assertEquals(3, ret.pop());
+        assertEquals(4, ret.pop());
+        assertEquals(5, ret.pop());
+        assertEquals(8, ret.pop());
     }
 
 }
