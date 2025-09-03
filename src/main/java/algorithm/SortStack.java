7be45c83e95baf385357d27cd6d7cdcba940631f
wlingxiao <wlingxiao@outlook.com>
2025-09-03 15:11:09 +0800

    更新注释

--- src/main/java/algorithm/SortStack.java
+++ src/main/java/algorithm/SortStack.java
@@ -13,7 +13,7 @@ public class SortStack {
     /// ##### 解决思路
     /// 汉诺塔问题
     /// 1. 弹出 stack 元素，如果 temp 为空则直接压入该元素
-    /// 2. 如果 temp 不为空，则检查 temp 顶部元素是否小于该元素，如果是则弹出 temp 顶部元素压入 stack，重复执行该操作，直到 temp 被清空。
+    /// 2. 如果 temp 不为空，则检查 temp 顶部元素是否小于该元素，如果是则弹出 temp 顶部元素压入 stack，重复执行该操作，直到 temp 顶部元素大于该元素或 temp 被清空。
     /// 3. 最后将该元素压入 temp
     public Stack<Integer> sort(Stack<Integer> stack) {
         val temp = new Stack<Integer>();
